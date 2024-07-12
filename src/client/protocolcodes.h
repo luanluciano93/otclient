7b5169dc66dc3cf9b85154a76030aee81c9463ab
Luan Luciano <luanluciano@outlook.com>
2024-07-11 23:12:54 -0300

    Update protocolcodes.h

--- src/client/protocolcodes.h
+++ src/client/protocolcodes.h
@@ -176,6 +176,7 @@ namespace Proto
         GameServerVipState = 211,
         GameServerVipLogout = 212,
         GameServerSendBestiaryEntryChanged = 217,
+        GameServerCyclopediaData = 218,
         GameServerTutorialHint = 220,
         GameServerAutomapFlag = 221,
         GameServerSendDailyRewardCollectionState = 222,
